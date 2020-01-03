26107768f2d6bd18a41d27c7d90e7a9e86687118
Gigitsu <gigitsu.23@gmail.com>
2020-01-03 12:29:00 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from codecs import open
 from os import path
 
-version = '0.0.1'
+version = '0.0.2'
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
